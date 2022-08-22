76f3110e9d200a878bafc1d62c2778b620fbdf13
Messahel Samy Salaheddine <messahel8samy23@gmail.com>
2022-08-22 12:55:24 +0100

    refresh ph

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ContriesApp from "../assets/portfolio/ph-contrie-app.png";
+import ContriesApp from "../assets/portfolio/ph-contrie-app2.png";
 import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
-import todolistApp from "../assets/portfolio/ph-todolist-app.png";
+import todolistApp from "../assets/portfolio/ph-todolist-app2.png";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
